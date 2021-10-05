e52fabf7ed68e7f4fc884cb421574d8be97cf0a4
oajh04 <cloudtubekr@naver.com>
2021-10-05 23:16:13 +0900

    ⚡️ :: 리덕스 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React, { Component } from "react";
+import withRedux from 'next-redux-wrapper';
+import { Provider } from 'react-redux'; 
+import { PersistGate } from 'redux-persist/integration/react';
+// import  configureStore from '../src/redux/store';
+import wrapper, { persistor } from '../src/redux/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <Component {...pageProps} />
+  )
+
 }
-export default MyApp
+export default wrapper.withRedux(MyApp)
