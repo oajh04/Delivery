a671cc2826fecdc4b3f305e90e633d8cb9856616
oajh04 <cloudtubekr@naver.com>
2021-10-05 23:20:34 +0900

    ⚡️ :: assets index

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { default as InTransit } from './InTransit.svg';
+export { default as AtPickup } from './AtPickup.svg';
+export { default as OutForDelivery } from './OutForDelivery.svg';
+export { default as Delivered } from './Delivered.svg';
+export { default as ReadyDelivery } from './ReadyDelivery.svg';
