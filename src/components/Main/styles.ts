ba41d2de63c77beb30720f6ea5e668eb889c4311
oajh04 <cloudtubekr@naver.com>
2021-10-05 23:19:00 +0900

    ⚡️ :: Main style

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const Wrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    max-width: 500px;
+    margin: 0 auto;
+    display: flex;
+    background-color: white;
+`
