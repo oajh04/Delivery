845019184b870156b443c5fbc764e3b74223a249
oajh04 <cloudtubekr@naver.com>
2021-10-06 23:38:31 +0900

    ⚡️ :: Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../redux/reducers';
 import Card from '../Card';
 import * as S from './styles'
 
 const Main = () => {
+
+  const data = useSelector((state: RootState)=> state.cardReducer.deliveres)
+
   return (
-      <Card />
+    <>
+      {
+        data?.map((i: any, index: React.Key | null | undefined) => {
+          return (
+            <Card key={index} {...i}/>
+          )
+        })
+      }
+    </>
   );
 }
 
