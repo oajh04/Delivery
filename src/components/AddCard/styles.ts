9388731585b3df82876f0d496b135e51e471e9ea
oajh04 <cloudtubekr@naver.com>
2021-10-06 19:40:54 +0900

    ⚡️ :: AddCard media 삭제

--- src/components/AddCard/styles.ts
+++ src/components/AddCard/styles.ts
@@ -1,11 +1,7 @@
 import styled from '@emotion/styled'
 
 export const Wrapper = styled.div`
-    max-width: 400px;
-    min-width: 300px;
     height: 128px;
-    width: 100%;
-    margin: 0 auto;
     border-radius: 8px;
     border: dashed 2px black;
     display: flex;
@@ -16,15 +12,6 @@ export const Wrapper = styled.div`
     svg{
         width: 40px;
         height: 40px;
-    }
-    @media screen and (max-width: 430px) { 
-        width: 350px;
-    }
-    @media screen and (max-width: 370px) { 
-        width: 320px;
-    }
-    @media screen and (max-width: 330px) { 
-        width: 300px;
     }
 
 
