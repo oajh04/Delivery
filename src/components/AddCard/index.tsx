c9df4e9c4214a6843696096bfcf858abe03286e5
oajh04 <cloudtubekr@naver.com>
2021-10-06 19:40:24 +0900

    ⚡️ :: Container Wrapper 추가

--- src/components/AddCard/index.tsx
+++ src/components/AddCard/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import * as S from './styles'
 import { BiPlusMedical } from 'react-icons/bi'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { openModal } from '../../redux/actions/addModalAction';
+import { RootState } from '../../redux/reducers';
+import { ContainerWrapper } from '../Container';
 
 const AddCard = () => {
   const dispatch = useDispatch();
@@ -13,9 +15,11 @@ const AddCard = () => {
 
   return (
     <>
+      <ContainerWrapper>
         <S.Wrapper onClick={onOpenModal}>
           <BiPlusMedical />
         </S.Wrapper>
+      </ContainerWrapper>  
     </>
   );
 }
