ab94f4d2f6df7955bd8fa5e794cbdd4589d3d158
oajh04 <cloudtubekr@naver.com>
2021-10-06 23:37:04 +0900

    ⚡️ :: addModalAction

--- src/redux/actions/addModalAction.ts
+++ src/redux/actions/addModalAction.ts
@@ -1,19 +1,22 @@
 import { createAction } from 'redux-actions'
 export const OPEN_MODAL = 'add/OPEN_MODAL';
 export const CLOSE_MODAL = 'add/CLOSE_MODAL'
+export const RESET_MODAL = 'add/RESET_MODAL';
 export const ITEM_NAME = 'add/ITEM_NAME';
 export const TRANSPORT_NUMBER = 'add/TRANSPORT_NUMBER'
 export const DELIVERY_COMPANY_ID = 'add/DELIVERY_COMPANY_ID';
 
 export const openModal = createAction(OPEN_MODAL);
 export const closeModal = createAction(CLOSE_MODAL);
-export const itemName = createAction(ITEM_NAME)
-export const transportNumber = createAction(TRANSPORT_NUMBER)
-export const deliveryId = createAction(DELIVERY_COMPANY_ID)
+export const resetModal = createAction(RESET_MODAL);
+export const itemName = createAction(ITEM_NAME);
+export const transportNumber = createAction(TRANSPORT_NUMBER);
+export const deliveryId = createAction(DELIVERY_COMPANY_ID);
 
 export type addModalActionType =
   | ReturnType<typeof openModal>
   | ReturnType<typeof closeModal>
+  | ReturnType<typeof resetModal>
   | ReturnType<typeof itemName>
   | ReturnType<typeof transportNumber>
   | ReturnType<typeof deliveryId>;
