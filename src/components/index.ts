0369938c3142c303912156318fe225eb2801af41
oajh04 <cloudtubekr@naver.com>
2021-10-06 19:44:40 +0900

    ⚡️ :: conponents index에 addModal 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Main } from './Main'
 export { default as Footer } from './Footer'
 export { default as AddCard } from './AddCard'
+export { default as AddModal } from './Modal/AddModal';
