bad605762a53c69c7785b274f5a67461e886978b
oajh04 <cloudtubekr@naver.com>
2021-10-06 19:42:24 +0900

    ⚡️ :: Container Wrapper Style

---
+++ src/components/Container/Wrapper/styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const ContainerWrapper = styled.div`
+    margin: 0 auto;
+    margin-bottom: 20px;
+    justify-content: center;
+    max-width: 400px;
+    min-width: 320px;
+    width: 100%;
+`
