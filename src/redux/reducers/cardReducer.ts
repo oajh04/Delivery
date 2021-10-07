4b5cef388898415bbc70072a094797f0fb365888
oajh04 <cloudtubekr@naver.com>
2021-10-07 11:29:28 +0900

    ⚡️ :: cardReducer action type 적용

--- src/redux/reducers/cardReducer.ts
+++ src/redux/reducers/cardReducer.ts
@@ -1,10 +1,10 @@
-import { ADD_CARD, DEL_CARD  } from '../actions/cardAction';
+import { ADD_CARD, cardActionType, DEL_CARD  } from '../actions/cardAction';
 
 const initalState: any = {
   deliveres: [],
 };
 
-export default function cardReducer(state = initalState, action: any): any {
+export default function cardReducer(state = initalState, action: cardActionType): any {
   switch (action.type) {
     case ADD_CARD: {
       return {
