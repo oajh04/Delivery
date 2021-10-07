ede2b9b8e81be1738912e5d0a70231abf5080892
oajh04 <cloudtubekr@naver.com>
2021-10-07 10:19:37 +0900

    ⚡️ :: main 애니메이션 추가

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { DelCard } from '..';
 import { RootState } from '../../redux/reducers';
 import Card from '../Card';
+import { motion } from "framer-motion";
 import * as S from './styles'
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const clickEvent = {
+  opacity: 1,
+  transition: 'all 0.2s linear',
+}
 
 const Main = () => {
+  const [ toggle, setToggle ] =useState<boolean>(false)
 
   const data = useSelector((state: RootState)=> state.cardReducer.deliveres)
 
@@ -13,7 +23,19 @@ const Main = () => {
       {
         data?.map((i: any, index: React.Key | null | undefined) => {
           return (
-            <Card key={index} {...i}/>
+            <motion.div
+              key={index}
+              onClick={() => setToggle(!toggle)}
+              style={clickEvent}
+              animate={toggle ? "active" : "inActive"}
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              {
+                !toggle ? <Card key={index} {...i}/>
+                : <DelCard />
+              }
+            </motion.div>
           )
         })
       }
