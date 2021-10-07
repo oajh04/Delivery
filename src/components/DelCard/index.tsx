6022be229dd14d7016d7a4c3780b73a4e6ea8c3d
oajh04 <cloudtubekr@naver.com>
2021-10-07 19:16:31 +0900

    ⚡️ :: DelCard toggle 추가

--- src/components/DelCard/index.tsx
+++ src/components/DelCard/index.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { ContainerWrapper } from '../Container';
 import * as S from './styles'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
 import { FcCancel } from 'react-icons/fc'
 import { useDispatch } from 'react-redux';
 import { closeMotion } from '../../redux/actions/motionAction';
+import { delCard } from '../../redux/actions/cardAction';
+import { useDeliveryId } from '../../libs/hooks/useDeliveryId';
 
-const DelCard = () => {
+interface Props {
+  data: any,
+  name: string,
+  id: number,
+  index: number,
+}
+
+const DelCard: FC<Props> = ({data, name, id, index}) => {
   const dispatch = useDispatch();
+  const toggleId = useDeliveryId(id, index)
+
+  const onDelete = () => {
+    dispatch(delCard(id))
+  }
 
   const onToggle = () => {
-    dispatch(closeMotion())
+    dispatch(closeMotion(toggleId))
   }
   return (
     <>
         <ContainerWrapper >
-            <S.Wrapper onClick={onToggle}>
-                <S.Button>
+            <S.Wrapper>
+                <S.Button onClick={onDelete}>
                   <AiOutlineCloseCircle />
                   <span>닫기</span>
                 </S.Button>
-                <S.Button>
+                <S.Button onClick={onToggle}>
                   <FcCancel />
                   <span>취소</span>
                 </S.Button>
