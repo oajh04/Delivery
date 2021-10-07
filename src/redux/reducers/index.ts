71629bebe44968e3f340552db4d6a31f36b68cbb
oajh04 <cloudtubekr@naver.com>
2021-10-07 22:18:19 +0900

    ⚡️ :: reducers index

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -3,17 +3,20 @@ import { combineReducers } from 'redux';
 import cardReducer from './cardReducer';
 import addModalReducer from './addModalReducer';
 import motionReducer from './motionReducer'
+import errorModalReducer from './errorModalReducer'
 
 export interface IReducerState {
     cardReducer: any,
     addModalReducer: any,
     motionReducer: any,
+    errorModalReducer: any,
 }
 
 const rootReducer = combineReducers({
   cardReducer,
   addModalReducer,
   motionReducer,
+  errorModalReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
