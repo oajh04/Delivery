85412eed256aa9ad4b3b211b87c7c25c1bb86221
oajh04 <cloudtubekr@naver.com>
2021-10-07 10:19:49 +0900

    ⚡️ :: Delete Card 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Main } from './Main'
 export { default as Footer } from './Footer'
 export { default as AddCard } from './AddCard'
+export { default as DelCard } from './DelCard'
 export { default as AddModal } from './Modal/AddModal';
