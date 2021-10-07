5799264ed9b0db7dd93cda700036522ea6ca21d6
oajh04 <cloudtubekr@naver.com>
2021-10-07 23:19:30 +0900

    ⚡️ :: _app 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
 import React, { Component } from "react";
-import withRedux from 'next-redux-wrapper';
-import { Provider } from 'react-redux'; 
-import { PersistGate } from 'redux-persist/integration/react';
-// import  configureStore from '../src/redux/store';
-import wrapper, { persistor } from '../src/redux/store'
+import {wrapper} from '../src/redux/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
