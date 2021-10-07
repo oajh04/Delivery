5a8e38731cbb896e51d51a411823ba4259e1d3ec
oajh04 <cloudtubekr@naver.com>
2021-10-07 19:17:17 +0900

    ⚡️ :: Main toggle 추가

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,27 +1,30 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { DelCard } from '..';
 import { RootState } from '../../redux/reducers';
 import Card from '../Card';
 import { motion } from "framer-motion";
+import { useDeliveryId } from '../../libs/hooks/useDeliveryId';
 
 const Main = () => {
-  const toggle = useSelector((state: RootState) => state.motionReducer.toggle)
+  const {toggle, toggleId} = useSelector((state: RootState) => state.motionReducer)
+  console.log(toggleId)
 
   const data = useSelector((state: RootState)=> state.cardReducer.deliveres)
 
   return (
     <>
       {
-        data?.map((i: any, index: React.Key | null | undefined) => {
+        data?.map((i: any, index: number) => {
           return (
             <motion.div
               key={index}
               whileTap={{ scale: 0.95 }}
             >
               {
-                !toggle ? <Card key={index} {...i}/>
-                : <DelCard />
+                !toggle || useDeliveryId(i.id, index) !== toggleId ? <Card key={index} {...i} index={index}/>
+                : <DelCard {...i} index={index}/>
               }
             </motion.div>
           )
