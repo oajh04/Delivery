b8695e1165389c91bcd59ea656869777091cc52d
oajh04 <cloudtubekr@naver.com>
2021-10-07 11:29:55 +0900

    ⚡️ :: RootReducer에 motionReducer 추가

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,15 +2,18 @@ import { combineReducers } from 'redux';
 
 import cardReducer from './cardReducer';
 import addModalReducer from './addModalReducer';
+import motionReducer from './motionReducer'
 
 export interface IReducerState {
-    cardReducer: any
-    addModalReducer: any
+    cardReducer: any,
+    addModalReducer: any,
+    motionReducer: any,
 }
 
 const rootReducer = combineReducers({
   cardReducer,
   addModalReducer,
+  motionReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
