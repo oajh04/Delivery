d2471db9a0d21da398a7a41b20efe2be42fd36a4
oajh04 <cloudtubekr@naver.com>
2021-10-08 23:34:36 +0900

    ⚡️ :: ErrorModal

--- src/components/Modal/ErrorModal/index.tsx
+++ src/components/Modal/ErrorModal/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ModalBaseContainer from '../ModalBase';
+import { motion } from 'framer-motion'
 import { BiErrorCircle } from 'react-icons/bi'
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../redux/reducers';
@@ -17,14 +18,24 @@ const ErrorModal = () => {
 
   return (
     <>
-        <ModalBaseContainer type='error' visibility={isError}>
+      <ModalBaseContainer type='error' visibility={isError}>
+        {
+          isError === 'visible' &&
+          <motion.div
+              initial={{ opacity: 0, scale: 0.75 }}
+              animate={{ opacity: 1, scale: 1}}
+              exit={{ opacity: 0, scale: 0 }}
+              transition={{ delay: 0.08 }}
+          >
           <S.Wrapper>
             <BiErrorCircle/>
             <div>{errorMsg}</div>
             <S.Button onClick={onClose}>닫기</S.Button>
           </S.Wrapper>
-        </ModalBaseContainer>
-    </>
+          </motion.div>
+        }
+      </ModalBaseContainer>
+  </>
   );
 }
 
