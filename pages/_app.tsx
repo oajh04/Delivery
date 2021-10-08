dc8471f43f0ad7cf7c297110abaf2a01f8036c13
oajh04 <cloudtubekr@naver.com>
2021-10-08 23:35:08 +0900

    ⚡️ :: Head 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import '../src/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import React, { Component } from "react";
 import {wrapper} from '../src/redux/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+      <Head>
+        <title>Delivery</title>
+        <link rel='icon' type='image/x-icon' href='/public/favicon.ico'></link>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
       <Component {...pageProps} />
+    </>
   )
 
 }
