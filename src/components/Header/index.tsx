93ffc91f94c1c2b6031f39cc75fb01303ce0d580
oajh04 <cloudtubekr@naver.com>
2021-10-08 23:34:17 +0900

    ⚡️ :: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ContainerWrapper } from '../Container';
+
+const Header = () => {
+  return (
+    <>
+        <ContainerWrapper>
+            
+        </ContainerWrapper>
+    </>
+  );
+}
+
+export default Header;
